806275d47b9fdd6fb1dd5f21c4a5af6b3adbf78a
Andrzejf <afornalik2@gmail.com>
2019-11-16 15:17:13 +0100

    testFrancMultiplication pass

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import tdd.utils.Dollar;
+import tdd.utils.Franc;
 
 import static org.junit.Assert.*;
 
@@ -13,6 +14,13 @@ public class DollarTest {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10),five.times(2));
         assertEquals(new Dollar(15),five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10),five.times(2));
+        assertEquals(new Franc(15),five.times(3));
     }
 
     @Test
