2fad17d3d1afb60c178e4ab5fdb1ec4b8e14ba31
Andrzejf <afornalik2@gmail.com>
2019-11-16 23:24:56 +0100

    Delete class Dollar and Franc. Remove some tests.

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import tdd.utils.Franc;
 import tdd.utils.Money;
 
 import static org.junit.Assert.*;
@@ -9,11 +8,6 @@ import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
 public class DollarTest {
 
-    @Test
-    public void testDifferentClassEquality(){
-        assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
-    
-    }
 
     @Test
     public void testCurrency() {
@@ -28,19 +22,11 @@ public class DollarTest {
         assertEquals(Money.dollar(15),five.times(3));
     }
 
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10),five.times(2));
-        assertEquals(Money.franc(15),five.times(3));
-    }
 
     @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 }
