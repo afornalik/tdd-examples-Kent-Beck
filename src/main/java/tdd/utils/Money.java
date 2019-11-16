3872089298651f13da88f60b42a66c8505533564
Andrzejf <afornalik2@gmail.com>
2019-11-16 16:31:56 +0100

    Create factory method in Money class

--- src/main/java/tdd/utils/Money.java
+++ src/main/java/tdd/utils/Money.java
@@ -1,13 +1,23 @@
 package tdd.utils;
 
-class Money {
+public abstract class Money {
 
     int amount;
 
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return this.amount == money.amount;
+        return this.amount == money.amount
+                && getClass().equals(obj.getClass());
     }
+
+    public abstract Money times(int multiplier);
 }
